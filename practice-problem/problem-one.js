f01bccaa1f9a3ecf663dd21c2f43a2593bc5bfbd
gautamsen117 <gautamsen117@gmail.com>
2022-10-07 16:30:48 +0600

    problem one

---
+++ practice-problem/problem-one.js
@@ -0,0 +1,13 @@
+// Practice problem 1:
+const multiply = (number1, number2, number3) => number1 * number2 * number3;
+const result = multiply(2, 3, 4);
+// console.log(result);
+// Practice problem 2:
+const threeLines = `I am a web developer.
+I love to code.
+I love to eat biryani.`;
+// console.log(threeLines);
+// Practice problem 3:
+const add = (first, second = 0) => first + second;
+const sum = add(10);
+console.log(sum);
